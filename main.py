0f6556822171a5ef27b92b2a6010c5d10a8d0a4b
Akshay M. Nair <31191266+akshaymnair@users.noreply.github.com>
2018-01-13 23:53:34 -0700

    update main
    hackAZ

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Sixth Sense- AI CCTV and Audio Monitoring System!')
 app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
